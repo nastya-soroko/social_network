7c96f435515cd188edac4c99718d40a6687f5c2a
nastya <soroko_a@tut.by>
2012-05-10 18:24:19 +0300

    finished routes & layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 SocialNetwork::Application.routes.draw do
+  get "users/new"
+  match '/signup',  :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
-
-
-
   root :to => 'pages#home'
   resources :users
 
